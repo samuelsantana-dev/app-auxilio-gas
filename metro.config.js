5ddd90805008e9a9d51350207a6b03ba4dc48b94
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-11-21 13:58:18 -0300

    Pacote de module resolver do babel, novo gitignore, novo metro, novo arquivo tailwind

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: 'app/style/style.css' })
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: "src/style/style.css" });
